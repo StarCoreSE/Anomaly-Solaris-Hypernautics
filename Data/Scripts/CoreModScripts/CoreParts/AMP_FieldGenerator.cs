5430aad98ba9c7a3163c30b640b0d4a562403231
FP-Anomaly <carbonjvd@gmail.com>
2023-06-14 22:19:22 -0700

    More Reload Sound Debugging

--- Data/Scripts/CoreModScripts/CoreParts/AMP_FieldGenerator.cs
+++ Data/Scripts/CoreModScripts/CoreParts/AMP_FieldGenerator.cs
@@ -143,10 +143,10 @@ namespace Scripts
                 },
                 Audio = new HardPointAudioDef
                 {
-                    PreFiringSound = "", // Audio for warmup effect.
+                    PreFiringSound = "MaxMemeReload", // Audio for warmup effect.
                     FiringSound = "shieldthing_firesound", // Audio for firing.
                     FiringSoundPerShot = true, // Whether to replay the sound for each shot, or just loop over the entire track while firing.
-                    ReloadSound = "MaxMemeReload", // Sound SubtypeID, for when your Weapon is in a reloading state
+                    ReloadSound = "", // Sound SubtypeID, for when your Weapon is in a reloading state
                     NoAmmoSound = "",
                     HardPointRotationSound = "WepTurretGatlingRotate", // Audio played when turret is moving.
                     BarrelRotationSound = "WepShipGatlingRotation",
