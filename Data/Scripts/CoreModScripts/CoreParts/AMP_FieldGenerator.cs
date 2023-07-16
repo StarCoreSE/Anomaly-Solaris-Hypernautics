c47a30c66894acb9610420e5cab077289bc7671e
InvalidArgument3 <InvalidArgument3@gmail.com>
2023-07-16 14:25:36 -0500

    also turn off heat

--- Data/Scripts/CoreModScripts/CoreParts/AMP_FieldGenerator.cs
+++ Data/Scripts/CoreModScripts/CoreParts/AMP_FieldGenerator.cs
@@ -127,7 +127,7 @@ namespace Scripts
                     DelayUntilFire = 0, // How long the weapon waits before shooting after being told to fire. Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     HeatPerShot = 0, // Heat generated per shot.
                     MaxHeat = 418, // Max heat before weapon enters cooldown (70% of max heat).
-                    Cooldown = .01f, // Percentage of max heat to be under to start firing again after overheat; accepts 0 - 0.95
+                    Cooldown = 10f, // Percentage of max heat to be under to start firing again after overheat; accepts 0 - 0.95
                     HeatSinkRate = 25, // Amount of heat lost per second.
                     DegradeRof = true, // Progressively lower rate of fire when over 80% heat threshold (80% of max heat).
                     ShotsInBurst = 10, // Use this if you don't want the weapon to fire an entire physical magazine in one go. Should not be more than your magazine capacity.
